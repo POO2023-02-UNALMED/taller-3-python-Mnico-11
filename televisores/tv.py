9db9eacc7b757aa2bf512db3b2aec4369a168abd
Mnico-11 <141962291+Mnico-11@users.noreply.github.com>
2023-08-28 20:47:44 -0500

    Update tv.py

--- televisores/tv.py
+++ televisores/tv.py
@@ -9,7 +9,7 @@ class TV:
             self._volumen = 1
             self._precio = 500
             self._control= None
-            TV._numTV += 1
+            TV.numTV += 1
 
     def setMarca(self, marca):
             self._marca = marca
